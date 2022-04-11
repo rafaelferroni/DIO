81f8ebd7b9d59cad6f69fc3d1666298cb311fb2e
rafaelferroni <rafaelferroni@gmail.com>
2022-04-10 21:20:09 -0300

    Variável corrijeString não atribui valor

---
+++ Troca números pares/script.js
@@ -0,0 +1,20 @@
+function trocaPares(string){
+    if(!string.length) return(console.log("-1"));
+    if(!string) return(console.log("-1"));    
+    let corrigeString = string.split(' ').join('');
+    
+    for(let i = 0; i <corrigeString.length; i++){
+        
+        if(corrigeString[i] == 0){
+            console.log("Você já é 0");
+        }else if(corrigeString[i] % 2 === 0){
+            //Pq a variável abaixo não atualiza o valor para 0?
+            console.log(`Substituindo ${corrigeString[i]} por 0...`);
+            corrigeString[i] = 0;
+        }
+    }
+    
+    return corrigeString;
+
+}
+console.log(trocaPares(prompt("Digite uma seuqencia de números separados por virgular")));
